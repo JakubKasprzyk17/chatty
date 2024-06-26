6a73c1fb3f5cf901936a7033b05149da9c1e4c1a
Jakub Kasprzyk <kasprzykjakub@proton.me>
2024-06-26 13:38:52 +0200

    chore: Add i18n support and language files

--- App.tsx
+++ App.tsx
@@ -1,9 +1,11 @@
 import React, { useCallback } from 'react';
 import { useFonts } from 'expo-font';
 import * as SplashScreen from 'expo-splash-screen';
+import { I18nextProvider } from 'react-i18next';
 
 import { NavigationContainer } from '@react-navigation/native';
 
+import { i18n } from '_locales';
 import { RootNavigator } from '_navigations';
 
 SplashScreen.preventAutoHideAsync();
@@ -27,9 +29,11 @@ const App = () => {
   }
 
   return (
-    <NavigationContainer>
-      <RootNavigator />
-    </NavigationContainer>
+    <I18nextProvider i18n={i18n}>
+      <NavigationContainer>
+        <RootNavigator />
+      </NavigationContainer>
+    </I18nextProvider>
   );
 };
 
---
+++ src/locales/i18n.ts
@@ -0,0 +1,26 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+
+import en from './en.json';
+
+const resources = {
+  en: {
+    translation: en,
+  },
+};
+
+i18n.use(initReactI18next).init({
+  compatibilityJSON: 'v3',
+  resources,
+  lng: 'en',
+  interpolation: {
+    escapeValue: false,
+  },
+  fallbackLng: {
+    default: ['en'],
+  },
+  fallbackNS: ['translation'],
+  debug: false,
+});
+
+export default i18n;
