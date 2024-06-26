bf52562912408f6db8cd013fa7a15bc18a9579ae
Jakub Kasprzyk <kasprzykjakub@proton.me>
2024-06-26 13:15:54 +0200

    chore: Optimize font loading and splash screen handling

--- App.tsx
+++ App.tsx
@@ -1,7 +1,28 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useCallback } from 'react';
+import { Text, View } from 'react-native';
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+
+SplashScreen.preventAutoHideAsync();
 
 const App = () => {
+  const [fontsLoaded, fontError] = useFonts({
+    'Poppins-Regular': require('./src/assets/fonts/Poppins-Regular.ttf'),
+    'Poppins-Medium': require('./src/assets/fonts/Poppins-Medium.ttf'),
+    'Poppins-SemiBold': require('./src/assets/fonts/Poppins-SemiBold.ttf'),
+    'Poppins-Bold': require('./src/assets/fonts/Poppins-Bold.ttf'),
+  });
+
+  useCallback(async () => {
+    if (fontsLoaded || fontError) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded, fontError]);
+
+  if (!fontsLoaded && !fontError) {
+    return null;
+  }
+
   return (
     <View>
       <Text>App</Text>
@@ -10,5 +31,3 @@ const App = () => {
 };
 
 export default App;
-
-const styles = StyleSheet.create({});
