91d0d37007f7822230254414f3f4aefee30e07a5
Jakub Kasprzyk <kasprzykjakub@proton.me>
2024-06-28 11:49:20 +0200

    feat: Update ChatList to ChatRooms and Chat screens

--- src/screens/index.ts
+++ src/screens/index.ts
@@ -1,3 +1,4 @@
 export { default as Login } from './auth/Login/Login';
 export { default as Register } from './auth/Register/Register';
-export { default as ChatList } from './chat/ChatList/ChatList';
+export { default as ChatRooms } from './chat/ChatRooms/ChatRooms';
+export { default as Chat } from './chat/Chat/Chat';
