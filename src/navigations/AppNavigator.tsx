40f04543681b074d4214e4db5a801ebfedd173b5
Jakub Kasprzyk <kasprzykjakub@proton.me>
2024-06-28 11:49:11 +0200

    feat: Update AppNavigator to include Chat and ChatRooms screens

--- src/navigations/AppNavigator.tsx
+++ src/navigations/AppNavigator.tsx
@@ -1,6 +1,6 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-import { ChatList } from '_screens';
+import { Chat, ChatRooms } from '_screens';
 import { AppNavigatorParamsList, AppRoutes } from '_types';
 
 const StackApp = createNativeStackNavigator<AppNavigatorParamsList>();
@@ -8,12 +8,13 @@ const StackApp = createNativeStackNavigator<AppNavigatorParamsList>();
 const AppNavigator = () => {
   return (
     <StackApp.Navigator
-      initialRouteName={AppRoutes.ChatList}
+      initialRouteName={AppRoutes.ChatRooms}
       screenOptions={{
         animation: 'fade_from_bottom',
         headerShown: false,
       }}>
-      <StackApp.Screen name={AppRoutes.ChatList} component={ChatList} />
+      <StackApp.Screen name={AppRoutes.ChatRooms} component={ChatRooms} />
+      <StackApp.Screen name={AppRoutes.Chat} component={Chat} />
     </StackApp.Navigator>
   );
 };
